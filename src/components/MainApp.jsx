78421694d4522e21f8d5838bd8b5a86edc401048
kierzkowska <paulina.kierz@gmail.com>
2023-01-26 16:40:37 +0100

    SecondStep

--- src/components/MainApp.jsx
+++ src/components/MainApp.jsx
@@ -3,6 +3,7 @@ import Axios from "axios";
 import Table from "react-bootstrap/Table";
 
 function MainApp() {
+  const index = 0;
   const [schedules, setSchedules] = useState([]);
   useEffect(() => {
     Axios.get(
@@ -13,32 +14,87 @@ function MainApp() {
     });
   }, []);
 
+  function HalfT(idN) {
+    const tabl = [];
+    for (let i = 0; i < schedules.length; i++) {
+      if (schedules[i].sport_event_status.status === "postponed") {
+        tabl.push("Postponed");
+      } else if (schedules[i].sport_event_status.status === "closed")
+        tabl.push(
+          schedules[i].sport_event_status.period_scores[0].home_score +
+            " : " +
+            schedules[i].sport_event_status.period_scores[0].away_score
+        );
+    }
+    return tabl[idN];
+  }
+
   return (
     <div>
       <Table striped bordered hover>
         <thead>
           <tr>
-            <th>Team Names</th>
+            <th>Team Names - Home</th>
+            <th>Team Names - Away</th>
             <th>Result</th>
+            <th>Match Date</th>
+            <th>Half time score</th>
+            <th>Stadium name</th>
           </tr>
         </thead>
         <tbody>
           {schedules.map((schedule, id) => {
             return (
               <tr key={id}>
-                <td>
-                  {schedule.sport_event.competitors[0].name} /{" "}
-                  {schedule.sport_event.competitors[1].name}
-                </td>
                 {schedule.sport_event_status.status === "postponed" ? (
-                  <td>{"Postponed"}</td>
+                  <td className="bg-secondary">
+                    {schedule.sport_event.competitors[index].name}
+                  </td>
+                ) : schedule.sport_event_status.match_tie ? (
+                  <td className="bg-warning">
+                    {schedule.sport_event.competitors[index].name}
+                  </td>
+                ) : schedule.sport_event.competitors[0].id ===
+                  schedule.sport_event_status.winner_id ? (
+                  <td className="bg-success">
+                    {schedule.sport_event.competitors[index].name}
+                  </td>
                 ) : (
-                  <td>
-                    {" "}
-                    {schedule.sport_event_status.home_score} :{" "}
-                    {schedule.sport_event_status.away_score}
+                  <td className="bg-danger">
+                    {schedule.sport_event.competitors[index].name}
                   </td>
                 )}
+
+                {schedule.sport_event_status.status === "postponed" ? (
+                  <td className="bg-secondary">
+                    {schedule.sport_event.competitors[1].name}
+                  </td>
+                ) : schedule.sport_event_status.match_tie ? (
+                  <td className="bg-warning">
+                    {schedule.sport_event.competitors[1].name}
+                  </td>
+                ) : schedule.sport_event.competitors[1].id ===
+                  schedule.sport_event_status.winner_id ? (
+                  <td className="bg-success">
+                    {schedule.sport_event.competitors[1].name}
+                  </td>
+                ) : (
+                  <td className="bg-danger">
+                    {schedule.sport_event.competitors[1].name}
+                  </td>
+                )}
+
+                <td>
+                  {schedule.sport_event_status.home_score} :{" "}
+                  {schedule.sport_event_status.away_score}
+                </td>
+                <td>
+                  {new Date(
+                    schedule.sport_event.start_time
+                  ).toLocaleDateString()}{" "}
+                </td>
+                <td>{HalfT(id)}</td>
+                <td>{schedule.sport_event.venue.name}</td>
               </tr>
             );
           })}
